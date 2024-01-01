ec4935b3df5d1611d8b4b29b00d1471371ae85b6
Abderrahmane <benjelloun857@gmail.com>
2024-01-01 23:46:56 +0100

    sort_algo1_selec_v1

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,11 +1,28 @@
 #include "sort.h"
 
 /**
- * @brief 
+ * @brief
  * selection_sort - function implementing the selection sort algorithm
  * @param array: pointer to the array
  * @param size : size of the array
  */
-void selection_sort(int *array, size_t size){
+void selection_sort(int *array, size_t size)
+{
+   size_t iMin, i, j, temp;
 
+    for (i = 0; i < size - 2; i++)
+    {
+        iMin = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[iMin] > array[j])
+            {
+                iMin = j;
+            }
+        }
+        temp = array[iMin];
+        array[iMin] = array[i];
+        array[i] = temp;
+        print_array(array, size);
+    }
 }
--- sort.h
+++ sort.h
@@ -16,6 +16,7 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+void selection_sort(int *array, size_t size);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 #endif
