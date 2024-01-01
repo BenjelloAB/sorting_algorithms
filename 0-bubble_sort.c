f028a6f1f24adb5c5ef883b40aec1a7f0f4395d1
Abderrahmane <benjelloun857@gmail.com>
2024-01-01 23:24:28 +0100

    setup_v1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+
+/**
+ * @brief 
+ * 
+ * bubble_sort - function that implement the bubble sort 
+ * @param array: pointer to array
+ * @param size : size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * @brief 
+ * selection_sort - function implementing the selection sort algorithm
+ * @param array: pointer to the array
+ * @param size : size of the array
+ */
+void selection_sort(int *array, size_t size){
+
+}
